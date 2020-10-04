cc88a7a1830c49afbeef39187ac95cad8c1d6ee1
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-04 17:11:54 +0200

    Add exercise 3 from lesson 2

---
+++ modulo-2-leccion-02-ejercicio-3/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+/* const kilogramsKiwi = 2;
+const kilogramsPears = 3;
+const kilogramsGrapes = 0.5;
+const priceKiwis = 5;
+const pricePears = 2;
+const priceGrapes = 4;
+const measurementUnits = "€/kg";
+
+console.log(
+  kilogramsKiwi * priceKiwis +
+    kilogramsPears * pricePears +
+    kilogramsGrapes * priceGrapes +
+    measurementUnits
+); */
+
+const kiwis = 2 * 5;
+const pears = 3 * 2;
+const grapes = 0.5 * 4;
+
+console.log(kiwis + pears + grapes + "€/kg");
