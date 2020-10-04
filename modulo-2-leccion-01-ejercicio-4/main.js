c5d3e8b76eeeb37bac4807ac0f7482f7385a3153
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-04 14:43:53 +0200

    Add exercise 4 from lesson 1

---
+++ modulo-2-leccion-01-ejercicio-4/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const name1 = document.querySelector(".name1");
+const name2 = document.querySelector(".name2");
+
+titleElement.innerHTML = titleElement.innerHTML + name2.innerHTML;
