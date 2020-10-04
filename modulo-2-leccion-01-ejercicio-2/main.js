8af40d8d9c75471bb106706150d8f6b55e7173e4
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-04 14:40:02 +0200

    Add exercise 2 from lesson 1

---
+++ modulo-2-leccion-01-ejercicio-2/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/* const houseAddress = "Sector Músicos, 14";
+houseAddress = "Sector Descubridores, 13";
+ */
+
+let houseAddress = "Sector Músicos, 14";
+houseAddress = "Sector Descubridores, 13";
