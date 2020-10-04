6c5ecc6a9b9f571bbaedb25297b381c2af1848c5
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-04 17:17:36 +0200

    Add exercise 9 from lesson 2

---
+++ modulo-2-leccion-02-ejercicio-9/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "Nuria Díaz";
+
+console.log(
+  "El nombre de mi compañera es " +
+    name +
+    " y está compuesto por " +
+    name.length +
+    " caracteres."
+);
