411865981ed67d9845555095837b50434e9d2aba
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-10 11:55:21 +0200

    Add exercise 2 from lesson 4

---
+++ modulo-2-leccion-04-ejercicio-2/main.js
@@ -0,0 +1,8 @@
+function average(a, b, c, d) {
+  const result = (a + b + c + d) / 4;
+  return result;
+}
+
+console.log(average(14, 54, 28, 24));
+console.log(average(2, 4, 6, 8));
+console.log(average(6, 34, 56, 18));
