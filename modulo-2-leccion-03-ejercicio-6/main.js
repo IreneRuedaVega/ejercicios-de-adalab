6f086cbd6fcda07cc56370a778d16706e1afddad
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-10 11:50:53 +0200

    Add exercise 6 from lesson 3

---
+++ modulo-2-leccion-03-ejercicio-6/main.js
@@ -0,0 +1,11 @@
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+
+/* if (money >= avocadoPrice) {
+  avocados = money / avocadoPrice;
+} else {
+  avocados = 0;
+} */
+
+avocados = money >= avocadoPrice ? money / avocadoPrice : (avocados = 0);
