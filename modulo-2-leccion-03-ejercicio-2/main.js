5499ed7fe6bdc4949f63517ef57f203032c6a7b1
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-10 11:43:07 +0200

    Add exercise 2 from lesson 3

---
+++ modulo-2-leccion-03-ejercicio-2/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const myName = "Irene";
+
+if (myName === "Irene") {
+  console.log("Bienvenida, Irene");
+} else {
+  console.log(
+    "Lo siento, pero el usuario que has introducido no está registrado."
+  );
+}
+
+/* const name = prompt("Introduce un nombre");
+
+if (name === "Emma") {
+  alert("Bienvenida Emma");
+} else {
+  alert("Lo siento pero el usuario que has introducido no está registrado");
+}
+ */
