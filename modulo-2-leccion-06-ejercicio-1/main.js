6f32d9b18f0cb337baf5b886ee9bf1b80f90d8e6
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-10 12:12:19 +0200

    Add exercise 1 from lesson 6

---
+++ modulo-2-leccion-06-ejercicio-1/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const adalaber1 = {
+  name: "Susana",
+  age: 34,
+  profession: "periodista",
+};
+
+const adalaber2 = {
+  name: "Rocío",
+  age: 25,
+  profession: "actriz",
+};
+
+console.log(
+  `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.profession}.`
+);
+
+console.log(
+  `${adalaber2.name} es una chica que tiene ${adalaber2.age} años y trabaja como ${adalaber2.profession}.`
+);
