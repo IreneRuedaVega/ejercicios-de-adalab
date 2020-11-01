ed5609f40963ad6aaa76dbc6b6a55beb1ab30641
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-01 21:40:44 +0100

    Add exercise 2 from lesson 10

---
+++ modulo-2-leccion-10-ejercicio-2/main.js
@@ -0,0 +1,14 @@
+function getDogImage() {
+  fetch("https://dog.ceo/api/breed/chihuahua/images/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const img = document.querySelector("img");
+      img.src = data.message;
+      img.alt = "Un perro";
+    });
+}
+const btn = document.querySelector(".js-dog");
+btn.addEventListener("click", getDogImage);
+
+const btn2 = document.querySelector(".js-chihuahua");
+btn2.addEventListener("click", getDogImage);
