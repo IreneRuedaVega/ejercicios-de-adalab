db4a5dd0f9760c2fac79ca29db620127697ea0b9
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-04 23:38:07 +0100

    Add exercise 1 from lesson 4

---
+++ modulo-3-leccion-04-ejercicio-1/my-react-project/src/components/Item.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Item extends React.Component {
+  render() {
+    return (
+      <div className="item">
+        <h5 className="quantity">{this.props.quantity}</h5>
+        <div>
+          <h5>{this.props.name}</h5>
+          <h6 className="text-muted">{this.props.description}</h6>
+        </div>
+        <div className="badge badge-info">{this.props.category}</div>
+        <h5 className="price">{this.props.price}€</h5>
+      </div>
+    );
+  }
+}
+
+Item.defaultProps = {
+  description: "No hay descripcion",
+  category: "No hay descripcion",
+};
+
+Item.propTypes = {
+  name: PropTypes.any.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default Item;
