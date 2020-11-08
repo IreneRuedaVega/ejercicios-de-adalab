3aa0d5cd9efd3db685df7bf8cbc9e579e26f9cff
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-11-08 21:26:35 +0100

    Add exercise 3 from lesson 5

--- modulo-3-leccion-05-ejercicio-3/my-react-project/src/components/OnionHater.js
+++ modulo-3-leccion-05-ejercicio-3/my-react-project/src/components/OnionHater.js
@@ -4,16 +4,22 @@ class OnionHater extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.isHating = false;
   }
 
   handleChange(event) {
-    /* if (event.target.value.toLowerCase().includes("cebolla ")) {
-      alert("Odio la cebolla");
-    } */
-    const value = event.target.value;
+    //Al convertir lo que introduzca el usuario en letras minúsculas, no importa cómo lo escriba (con mayúsculas o no)
+    //utilizamos toLowerCase()
+    const value = event.target.value.toLowerCase();
+    const windowColor = document.querySelector(".App");
     if (value.includes("cebolla")) {
-      alert("ODIO LA CEBOLLA");
+      this.isHating = true;
+      windowColor.classList.add("red");
+    } else {
+      this.isHating = false;
+      windowColor.classList.remove("red");
     }
+    this.forceUpdate();
   }
 
   render() {
